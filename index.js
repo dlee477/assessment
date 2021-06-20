f000eb8e70c008a121c27f468f24b085b6b26b08
Derek <dlee477@my.bcit.ca>
2021-06-20 13:34:07 -0700

    fix bug with unable to upload from anywhere

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const uploadController = require('./controller/upload');
-
-router.post('/upload', uploadController.upload);
-module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const route = require('./index');
 const expressHbs = require('express-handlebars');
 var express = require('express');
-const bodyParser = require('body-parser');
+const AWS = require('aws-sdk');
 var app = express();
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+const BUCKET_NAME = 'bcit-test';
+const fs = require('fs');
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended:true}));
+// app.use(bodyParser.json())
 app.engine(
     'hbs',
     expressHbs({
@@ -19,11 +20,35 @@ app.engine(
   );
   app.set('view engine', 'hbs');
   app.set('views', 'views')
-app.use(express.static('public'));
-app.use(route);
+// app.use(express.static('public'));
+// app.use(route);
 
 app.get('/', (req,res)=> {
     res.render('upload')
+})
+
+
+app.post('/upload', upload.single('filename'), (req,res) => {
+  console.log(req.file.originalname);
+  fileContent = fs.readFileSync("./uploads/" +  req.file.filename)
+  const s3 = new AWS.S3();
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: req.file.originalname,
+    Body: fileContent
+}
+
+  s3.upload(params,function(err,data){
+    if(err){
+      console.log('fail2')
+      throw err;
+  }
+    console.log("success");
+    res.json({
+        message: "file uploaded to S3",
+        'location': data.Location
+    })
+  })
 })
 
 app.listen(3000, function () {
